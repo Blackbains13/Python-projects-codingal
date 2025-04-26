850696a08e600083555de6008dbd09b82c6b438a
Blackbains13 <kwasiblackbains@gmail.com>
2025-04-26 12:28:31 +0000

    Add files via upload

---
+++ Logarithm.py
@@ -0,0 +1,12 @@
+import math
+
+
+number = int(input("Enter the number:"))
+base = int(input("Enter the base:"))
+
+
+log_value = math.log(number, base)
+
+
+print("The logarithim of",number, 'with base',base,'is' , log_value)
+print(3**2)

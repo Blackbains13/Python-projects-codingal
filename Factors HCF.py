f9f3a1bb5831d4d244c8aed60bf253f2e049584f
Blackbains13 <kwasiblackbains@gmail.com>
2025-05-03 13:04:14 +0000

    Add files via upload

---
+++ Factors HCF.py
@@ -0,0 +1,7 @@
+numberLargest = int(input("Enter the largest number: "))
+numberSmallest = int(input("Enter the smallest number: "))
+while numberSmallest:
+    numberStore = numberSmallest
+numberSmallest = numberLargest % numberSmallest
+numberLargest = numberStore
+print("HCF is: ", numberLargest)

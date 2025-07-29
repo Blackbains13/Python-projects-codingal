f055c72ce066eea26ce04309475e723d9b39a161
Blackbains13 <kwasiblackbains@gmail.com>
2025-07-29 09:51:59 +0000

    Add files via upload

---
+++ newpro.py
@@ -0,0 +1,19 @@
+
+import sqlite3
+from tabulate import tabulate
+# Connect to the database
+conn = sqlite3.connect("weather.db")
+cursor = conn.cursor()
+# Update rows where weather_type is 'Rain'
+cursor.execute("""
+UPDATE weather
+SET is_holiday = 'no'
+WHERE weather_type = 'Mist'
+""")
+# Commit changes
+conn.commit()
+# Optional: Show the updated table
+cursor.execute("SELECT * FROM weather WHERE weather_type IN('Rain','Mist','Drizzle' From weather")
+rows = cursor.fetchall()
+print(tabulate(rows, headers=[i[0] for i in cursor.description]))
+
